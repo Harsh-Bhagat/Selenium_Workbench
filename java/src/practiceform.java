e6c18d015ab6e7613dfd61258e6ecb89009ac79d
Harsh-Bhagat <harshb8294@gmail.com>
2025-01-04 19:45:22 +0530

    Update and rename Sample12.java to practiceform.java

--- java/src/practiceform.java
+++ java/src/practiceform.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Sample12 {
+public class practiceform {
 
 	public static void main(String[] args) throws InterruptedException
 	{

55344b372e22780e392928aa6695b1965d76e6f1
Harsh-Bhagat <harshb8294@gmail.com>
2025-01-04 19:58:27 +0530

    Update and rename Sample23.java to facebooksignup.java

--- java/src/Generic/facebooksignup.java
+++ java/src/Generic/facebooksignup.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
-public class Sample23 {
+public class facebooksignup {
 
 	public static void main(String[] args) 
 	{

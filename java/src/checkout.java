ad1b4e7c8e4a4c9fa0bcf3926636007b9c02a25d
Harsh-Bhagat <harshb8294@gmail.com>
2025-01-04 19:52:36 +0530

    Update and rename Sample7.java to checkout.java

--- java/src/checkout.java
+++ java/src/checkout.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Sample7 {
+public class checkout {
 
 	public static void main(String[] args){
 
